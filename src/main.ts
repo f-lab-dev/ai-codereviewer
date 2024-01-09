50a16a407a7105ad3c27c92be262513b6bac0bd2
f-lab-eden <eden@f-lab.kr>
2024-01-09 17:08:44 +0900

    끼이잉 ..

--- src/main.ts
+++ src/main.ts
@@ -81,12 +81,22 @@ async function analyzeCode(
   }
   return comments;
 }
-
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   const prompt = `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
-- THIS IS TEST
-
+- Do not give positive comments or compliments.
+- Do not give naming convention comments or compliments.
+- Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
+- Provide up to 3 comments per file.
+- Only provide one comment or suggestion on a similar topic.
+- Comments and suggestions only provide for performance, maintenance, and best practices.
+- Write the comment in GitHub Markdown format.
+- Use the given description only for the overall context and only comment the code.
+- Please explain the reason for the comments and suggestion in as much detail as possible so that the requestor can learn deeply.
+- Please check carefully if the line number of the comments is correct.
+- If you have a similar comments and suggestions, please provide it only once.
+- Comments are provided in Korean.
+- IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
   
